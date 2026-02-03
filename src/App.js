650e5855eb45bba2592b24eca5fa6fbb3811a1ae
Enso Mugno Galvis <ensomugnog@gmail.com>
2026-02-04 09:49:19 +1000

    08 - Introducing the State System

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
 function App() {
+
+    const [count, setCount] = useState(0);
+
     const handleClick = () => {
-        console.log('Button was clicked!');
+        setCount(count + 1);
     };
 
     return (
         <div>
             <button onClick={handleClick}>Add animal</button>
+            <div>
+                Number of animals: {count}
+            </div>
         </div>
     );
 }
